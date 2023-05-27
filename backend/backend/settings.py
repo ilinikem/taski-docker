66698d4d788248d1584f22176c262280a3420aee
Maksim Sekletov <ti-jey89@mail.ru>
2023-05-27 17:34:27 +0700

    fix allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.69.13', '127.0.0.1', 'localhost', 'todo.serveirc.com']
 
 
 # Application definition
